290e667369f25750c4f6b19f1e53c4338c385d33
Yunlu Wen <yunlu.wen@transwarp.io>
2021-03-11 12:00:52 +0800

    improve client

--- client/client_test.go
+++ client/client_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 const config = "/home/transwarp/.kube/config"
-const testNs = "makaveli"
+const testNs = "test"
 
 func Test(t *testing.T) {
 	config, err := clientcmd.BuildConfigFromFlags("", config)
@@ -23,11 +23,23 @@ func Test(t *testing.T) {
 		log.Println(err)
 		return
 	}
-	pf := v1.PathFinderList{}
-	err = pfclient.PathFinderV1(testNs).List(context.TODO(), &pf, PathFinderListOption{})
+	pfl := v1.PathFinderList{}
+	err = pfclient.PathFinderV1(testNs).List(context.TODO(), &pfl, PathFinderListOption{})
 	if err != nil {
 		log.Println(err)
 	}
+	log.Println(pfl)
+
+	pf := v1.PathFinder{}
+	err = pfclient.PathFinderV1(testNs).Get(context.TODO(), "pathfinder-sample", &pf)
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Println(pf)
 
+	err = pfclient.PathFinderV1(testNs).GetByRegion(context.TODO(), "DEFAULT", &pf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(pf)
 }
